087274ae0f7a2dd0895e9ae7dcd232e9673d6116
purvesh-shingala <purveshshingala96@gmail.com>
2023-04-29 17:37:10 +0530

    areac

---
+++ REV_22_2.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+
+{
+int r,area;
+const pi=3.14;
+clrscr();
+printf("R = ");
+scanf("%d",&r);
+ area=pi*r*r;
+printf("AREA of circle is =  %d",area);
+getch();
+}
+
+
